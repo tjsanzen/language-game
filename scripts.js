17abed3974cbbbd4e8ce11d6c8fa1d79f4f42523
Tovin <tjsanzen@gmail.com>
2022-04-18 23:37:51 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
-//variables
+
 var quiz = [];
+
+// table of all the posible quiz questions and answers
 quiz[0] = new Question("Hola", "languages/spanish.mp3", "Spanish", "Italian", "Cyric");
 quiz[1] = new Question("Hello", "languages/english.mp3", "English", "Spanish", "French");
 quiz[2] = new Question("Haye", "languages/udhr_somali.mp3", "Somali", "Norwegian", "Swedish");
+
 var randomQuestion;
 var answers = [];
 var currentScore = 0;
@@ -11,6 +14,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   btnProvideQuestion();
 });
 
+
+// read which parts of each string are what
 function Question(question,audio,rightAnswer,wrongAnswer1,wrongAnswer2) {
     this.question = question;
     this.audio = audio;
@@ -19,6 +24,7 @@ function Question(question,audio,rightAnswer,wrongAnswer1,wrongAnswer2) {
     this.wrongAnswer2 = wrongAnswer2;
 };
 
+// math to shuffle
 function shuffle(o) {
 	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
 	return o;
@@ -31,7 +37,9 @@ function btnProvideQuestion() {
   answers = [randomQuestion.rightAnswer, randomQuestion.wrongAnswer1, randomQuestion.wrongAnswer2];
   shuffle(answers);
   
+// define the parts of the question further i think
   document.getElementById("question").innerHTML= randomQuestion.question;
+  document.getElementById("audio").innerHTML=randomQuestion.audio;
   document.getElementById("answerA").value= answers[0];
   document.getElementById("answerA").innerHTML= answers[0];
   document.getElementById("answerB").value= answers[1];
@@ -41,26 +49,31 @@ function btnProvideQuestion() {
 
 }
 
+// what do when play audio button clicked
 function audio_clicked() {
-  var audio = document.getElementById("audio").value;
-  audio.play()
+  var audio = document.getElementById("audio").play;
 }
 
+// what to do when answer a button clicked
 function answerA_clicked() {
   var answerA = document.getElementById("answerA").value;
   	checkAnswer(answerA);
 }
 
+// what to do when answer b button clicked
 function answerB_clicked() {
 		var answerB = document.getElementById("answerB").value;
   checkAnswer(answerB);
 }
+
+// what to do when answer c button clicked
 function answerC_clicked() {
   var answerC = document.getElementById("answerC").value;
   	
 		checkAnswer(answerC);
 }
 
+// check if correct to adjust score
 function adjustScore(isCorrect) {
   debugger;
   if (isCorrect) {
@@ -73,6 +86,7 @@ function adjustScore(isCorrect) {
   document.getElementById("score").innerHTML = currentScore;
 }
 
+// what to do if it is correct or incorrect
 function checkAnswer(answer) {  
   if (answer == randomQuestion.rightAnswer) {
     adjustScore(true);
